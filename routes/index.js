ac8327442a06cb18c5b8368df2163d48894a4331
kyedubati <113061205+kyedubati@users.noreply.github.com>
2022-10-12 13:51:25 -0500

    intialized routing protocal for login and register page, installed post logic and imported data through body-parser and commited that to mongo data, futher aut henticated user to checkin in to site completing proper signing in

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,52 @@
 const express       = require("express");
 const router        = express.Router();
+const passport      = require("passport");
+const User          = require("../models/user");
 
 // ========== Landing ========== //
 
 router.get("/", function (req, res) {
     res.render("landing");
 })
+
+
+// ========== signup Form ===========
+
+router.get("/register", function (req, res) {
+    res.render("register", {page: 'register'})
+})
+
+
+//  sign up Logic ==========
+
+router.post("/register", function (req, res) {
+    const newUser = new User({
+        username: req.body.username
+    });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            return res.redirect("/register");
+        }   
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/dashboard");
+        })
+    })
+})
+
+//  Login Form ==========
+
+router.get("/login", function (req, res) {
+    res.render("login", {page: 'login'});
+})
+
+//  Login logic =============
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    
+failureFlash: true
+}), function (req, res) {});
+
 
 module.exports = router;
