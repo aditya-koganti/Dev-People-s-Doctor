ee1f2b8dead9bbb949f6afb06319b6e222f24ce0
BhanuKandula5030 <k.bhannuprasad@gmail.com>
2022-10-26 00:03:53 +0530

    Model for doctor was created, attributes assigned were name, rating, specialities and finally description,   form data will be sent through post method as an json object under the four space names above, route assigned to '/' which would be '/doctor' as condensed to maintain under connecting file.

--- routes/doctor.js
+++ routes/doctor.js
@@ -7,4 +7,23 @@ router.get("/doctorForm", function(req, res){
     res.render("doctors/doctorForm.ejs");
 })
 
+router.post("/", function(req, res){
+    Doctor.create({
+        name: req.body.name,
+        rating: req.body.rating,
+        specialities: req.body.specialities,
+        description: req.body.description}, function(err, newDoctor){
+            if(err){
+                console.log("Doctor creation error!")
+                console.log(err);
+            }else{
+                console.log("Doctor Created");
+                console.log(newDoctor);
+            }
+        }
+    )
+})
+
+
+
 module.exports = router;
