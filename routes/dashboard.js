9c5f95476ca772bd819ba870667fb5f839199b45
kogantiaditya <aditya.koganti@slu.edu>
2022-10-26 02:55:54 -0500

    A section was implemented in profile page of doctor, given references to appoitment page and information submmission, link was under the route of dashboard, A bootstrap sytled info form was created, added some inputs for testing, route will directed under the id of the doctors passing through params

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const doctor = require("../models/doctor");
 const router = express.Router();
 
 router.get("/", function(req, res){
     res.render("dashboards/index.ejs")
+})
+
+router.get("/informationForm/:id", function(req, res){
+    var doctorID = req.params.id;
+    res.render("dashboards/informationForm", {doctorID: doctorID});
 })
 
 module.exports = router;
