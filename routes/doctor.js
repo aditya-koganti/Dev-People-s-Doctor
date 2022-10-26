e07c55d5b915fc10db3e52308f82440d20aafd03
kyedubati <113061205+kyedubati@users.noreply.github.com>
2022-10-25 20:43:14 -0500

    since we are retriveing doctors based departments, attribute of string was added to doctor schema; doctor creation route was updated, similarly added input tags of department to form page and display page; and comming to main part, sending the post query through '/doctor/doctors', filtering the doctors by doctor name obtained from dropdown and passing the data and displaying it through file named doctors.ejs under route '/doctor/doctors'

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,38 +1,60 @@
 const express = require("express"),
-app = express();
+  app = express();
 const router = require(".");
 const Doctor = require("../models/doctor");
 
-router.get("/doctorForm", function(req, res){
-    res.render("doctors/doctorForm.ejs");
-})
+router.get("/doctorForm", function (req, res) {
+  res.render("doctors/doctorForm.ejs");
+});
 
-router.post("/", function(req, res){
-    Doctor.create({
-        name: req.body.name,
-        rating: req.body.rating,
-        specialities: req.body.specialities,
-        description: req.body.description}, function(err, newDoctor){
-            if(err){
-                console.log("Doctor creation error!")
-                console.log(err);
-            }else{
-                console.log("Doctor Created");
-                console.log(newDoctor);
-            }
-        }
-    )
-})
+router.post("/", function (req, res) {
+  Doctor.create(
+    {
+      name: req.body.name,
+      department: req.body.department,
+      rating: req.body.rating,
+      specialities: req.body.specialities,
+      description: req.body.description,
+    },
+    function (err, newDoctor) {
+      if (err) {
+        console.log("Doctor creation error!");
+        console.log(err);
+      } else {
+        console.log("Doctor Created");
+        console.log(newDoctor);
+        res.redirect("/doctor/all");
+      }
+    }
+  );
+});
 
-router.get("/all", function(req, res){
-    Doctor.find({}, function(err, allDoctors){
-        if(err){
-            console.log("All doctors retrieval error");
-            console.log(err)
-        }else{
-            res.render("doctors/allDoctors.ejs", {allDoctors: allDoctors})            
-        }
-    })
-})
+router.get("/all", function (req, res) {
+  Doctor.find({}, function (err, allDoctors) {
+    if (err) {
+      console.log("All doctors retrieval error");
+      console.log(err);
+    } else {
+      res.render("doctors/allDoctors.ejs", { allDoctors: allDoctors });
+    }
+  });
+});
+
+router.post("/doctors", function (req, res) {
+  var departmentName = req.body.departmentName;
+  // finding all doctors of same department, and ---- { $regex: departmentName, $options: 'i' } ---
+  //   ----- to make mongoose search without considering smallercase or uppercase
+  Doctor.find(
+    { department: { $regex: departmentName, $options: "i" } },
+    function (err, doctors) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(doctors);
+        res.render("doctors/doctors", { doctors: doctors, departmentName: departmentName });
+      }
+    }
+  );
+});
 
 module.exports = router;
