f19d2e560a93fd01c7b0a5192c86a0b9f9708b72
kogantiaditya <aditya.koganti@slu.edu>
2022-11-18 21:01:28 -0600

    define route logic for pushing disease data to db

--- routes/disease.js
+++ routes/disease.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
-// const Disease = require("../models/disease");
+const Disease = require("../models/disease");
 
 router.get("/diseaseForm", function(req, res){
     res.render("disease/diseasesForm")
 })
+
+router.post("/disease", function(req, res){
+    var disease = req.body.disease;
+    var symptoms = req.body.symptoms;
+    var newDisease = {
+        disease: disease,
+        symptoms: symptoms
+    }
+    Disease.create(newDisease, function(err, createdDisease){
+        if(err){console.log("created error");console.log(err)}
+        else{
+            req.flash("success","You have created a disease");
+            console.log(createdDisease);
+            res.redirect("/diseases");
+        }
+    })
+})
+
 
 module.exports = router;
